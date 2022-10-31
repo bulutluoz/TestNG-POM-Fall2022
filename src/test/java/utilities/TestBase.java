237c3caff3a2d16f2ed1948289b9a7736be8777e
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-10-31 21:47:58 +0100

    14.gun sonu.

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,11 +1,16 @@
 package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class TestBase {
@@ -25,5 +30,25 @@ public class TestBase {
     @AfterMethod
     public void teardown(){
         driver.close();
+    }
+
+    public void bekle(int saniye){
+
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void fotografCek(String resimIsmi){
+        TakesScreenshot tss= (TakesScreenshot) driver;
+        File resimDosyasi= new File("target/screnShots/"+resimIsmi+".jpeg");
+        File geciciResim=tss.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(geciciResim,resimDosyasi);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
