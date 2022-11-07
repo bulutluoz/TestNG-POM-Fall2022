10ac71413f2ba0021bf20040199eb3c7554d00dd
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-11-07 19:20:58 +0100

    17.ders sonu.

---
+++ src/test/java/tests/smokeTests/PozitifLoginTesti.java
@@ -0,0 +1,17 @@
+package tests.smokeTests;
+
+import org.testng.annotations.Test;
+
+public class PozitifLoginTesti {
+
+
+    @Test
+    public void pozitifLoginTesti(){
+        // Mycoursedemy anasayfasina gidin
+        // login linkine basin
+        // Kullanici email'i olarak valid email girin
+        // Kullanici sifresi olarak valid sifre girin
+        // Login butonuna basarak login olun
+        // Basarili olarak giris yapilabildigini test edin
+    }
+}
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -10,6 +10,11 @@ import java.time.Duration;
 
 public class Driver {
 
+    private Driver(){
+        // Singleton pattern konsepti ile
+        // Driver class'indan obje olusturmayi engellemek icin bu cons.'i olusturduk
+    }
+
 
     static WebDriver driver;
     public static WebDriver getDriver(){
