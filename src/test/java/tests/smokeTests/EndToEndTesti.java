2ed56587cfd0452729a0038b0cb7c03d4fdd6132
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-11-07 20:16:33 +0100

    17.ders sonu.

---
+++ src/test/java/tests/smokeTests/EndToEndTesti.java
@@ -0,0 +1,14 @@
+package tests.smokeTests;
+
+public class EndToEndTesti {
+    /*
+      E2E (End to end testi, uctan uca bir islevin tum asamalarini
+      gerceklestirerek, islevin calistigini test etmek demektir
+
+      // MyCoursedemy sayfasina gidin
+      // gecerli kullanici adi ve sifre girerek sisteme login olun
+      // MyCourses linkini tiklayin
+      // yeni bir course olusturmak icin istenen bilgileri girin
+      // yeni kursun olusturuldugunu test edin
+     */
+}
