8e1df70eced3ef826ebc355ba633ab28357ce1c8
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-11-12 10:13:13 +0100

    18.ders sonu.

--- src/test/java/tests/day18_testNGReports_paralelTesting/C03_RaporluTestOlusturma.java
+++ src/test/java/tests/day18_testNGReports_paralelTesting/C03_RaporluTestOlusturma.java
@@ -8,32 +8,30 @@ import pages.AmazonPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
 import java.util.List;
 
-public class C02_ReusableMethodsElementList {
+public class C03_RaporluTestOlusturma extends TestBaseRapor {
 
     @Test
     public void test01(){
+        extentTest=extentReports.createTest("Dropdown Testi","Dropdown'da istenen element oldugunu test etme");
         // amazon anasayfaya gidin
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        extentTest.info("amazon ansayfaya gidildi");
         // arama kutusunun yanindaki dropdown menuyu locate edin
         // dropdown menudeki tum options listesini olusturup
         AmazonPage amazonPage=new AmazonPage();
         Select select=new Select(amazonPage.dropdownMenu);
+        extentTest.info("Dropdown menu locate edildi");
         List<WebElement> optionsElementList= select.getOptions();
 
         // menude Electronics basligi oldugunu test edin
-
-        // optionsElementList webElement'lerden olusuyor
-        // Electronics String'inin listede olup olmadigini test etmek icin
-        // oncelikle String'lerden olusan bir liste olmali
-
         List<String> optionsStringListesi= ReusableMethods.getElementsText(optionsElementList);
-
+        extentTest.info("Dropdown menudeki tum option'lar bir list olarak kaydedildi");
         Assert.assertTrue(optionsStringListesi.contains(ConfigReader.getProperty("dropdownArananOption")));
+        extentTest.pass("Dropdown menude aranan kelime oldugu test edildi");
         Driver.closeDriver();
-
-
     }
 }
